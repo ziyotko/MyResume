b3fbda8d5b57942647d8d7ad923f92126195a7c1
zhitianbai <zhitianbai@163.com>
2022-06-22 22:47:08 +0800

    Initial Commit

---
+++ MyResume/MyResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyResumeApp.swift
+//  MyResume
+//
+//  Created by 支天白 on 2022/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MyResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
